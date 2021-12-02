f1a272248a854cc40ab2c120f40b5a00cecee117
Rogelio Mohigefer Barrera <rogelio.mb@hotmail.com>
2021-12-02 10:24:32 +0100

    cambios data

--- LosLudopatasGrupo4/src/datos/DatosRogelio.java
+++ LosLudopatasGrupo4/src/datos/DatosRogelio.java
@@ -2,4 +2,38 @@ package datos;
 
 public class DatosRogelio {
 
+	int [] Ruleta = new int [] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
+			 31, 32, 33, 34, 35, 36};
+	
+	String [] JugadorUno = new String [];
+	
+	public static void StringNombre () {
+		String nombreJugUno = "";
+	}
+	public static void StringApellidoP () {
+		String apellidoPDos = "";
+	}
+	public static void StringApellidoM () {
+		String apellidoMTres = "";
+	}
+	
+	
+	/*Números Ruleta*/
+	
+	public void String () {
+		return;
+	}
+	public static void StringUno () {
+		String uno = "1";
+	}
+	public static void StringDos () {
+		String dos = "2";
+	}
+	public static void StringTres () {
+		String tres = "3";
+	}
+	public static void StringCuatro () {
+		String cuatro = "4";
+	}
+
 }
