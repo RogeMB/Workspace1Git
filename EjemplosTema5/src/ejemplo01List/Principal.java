a41e71f651edf0f848d0ce6244aa5a80452ef2fa
Rogelio Mohigefer Barrera <rogelio.mb@hotmail.com>
2022-02-21 10:53:00 +0100

    Empezando tema 5 ejemplo arraylist

---
+++ EjemplosTema5/src/ejemplo01List/Principal.java
@@ -0,0 +1,26 @@
+package ejemplo01List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List <String>lista = new ArrayList<String> ();
+		List <Integer>enumerados = new ArrayList<Integer> ();		
+		
+		lista.add("Roge");
+		lista.add("Cosi");
+		
+		
+		enumerados.add(2);
+		
+		
+		System.out.println(lista);
+		
+		
+	}
+
+}
