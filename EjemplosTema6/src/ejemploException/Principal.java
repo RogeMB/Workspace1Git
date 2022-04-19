28b545c92d823d0c904364d2c73a27b9dfeaa7de
Rogelio Mohigefer Barrera <rogelio.mb@hotmail.com>
2022-04-19 14:29:22 +0200

    si

---
+++ EjemplosTema6/src/ejemploException/Principal.java
@@ -0,0 +1,70 @@
+package ejemploException;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Principal {
+	
+	// El método dividir no trata la excepción con lo que debe lanzarla (throws)
+	public static int dividir(int numerator, int denominator) throws ArithmeticException {
+		return numerator / denominator; // possible division by zero
+	} // end method quotient
+
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+		
+		int num, den;
+		String aux;
+		
+		Scanner sc = new Scanner(System.in); // scanner for input
+		boolean continueLoop = true; // determines if more input is needed
+		do {
+			try {// read two numbers and calculate quotient
+
+				/*
+				 * System.out.print("Please enter an integer numerator: ");
+				 * int numerator = scanner.nextInt();
+				 * System.out.print("Please enter an integer denominator: ");
+				 * int denominator = scanner.nextInt();
+				 */
+				
+				System.out.print("Please enter an integer numerator: ");
+				aux=sc.nextLine();
+				num = Integer.parseInt(aux);
+				System.out.print("Please enter an integer denominator: ");
+				aux=sc.nextLine();
+				den = Integer.parseInt(aux);
+				
+				int result = dividir(num, den);
+				System.out.printf("\nResult: %d / %d = %d\n", num, den, result);
+				continueLoop = false; // input successful; end looping
+				
+				/*
+				 * int result = dividir(numerator, denominator);
+				 * System.out.printf("\nResult: %d / %d = %d\n", numerator, denominator, result);
+				 * continueLoop = false; // input successful; end looping
+				 */
+				
+			} // end try
+			
+			//Los catch se ordenan por concreción. El más genérico el último. El catch más concreto el primero.
+			catch (InputMismatchException inputMismatchException) {
+				System.err.printf("\nException: %s\n", inputMismatchException);
+				sc.nextLine(); // discard input so user can try again
+				System.out.println("You must enter integers. Please try again.\n");
+			} // end catch
+
+			catch (ArithmeticException arithmeticException) {
+				System.err.printf("\nException: %s\n", arithmeticException);
+				System.out.println("Zero is an invalid denominator. Please try again.\n");
+			} // end catch
+			
+			catch (NumberFormatException numberFormatException) {
+				System.err.printf("\nException: %s\n", numberFormatException);
+				System.out.println("Yoy must enter numbers. Please try again.\n");
+			}
+			
+		} while (continueLoop); // end do...while
+		sc.close();
+	} // end main	
+}
